b2132e8620ca980930b71ec5405ea94800c06e41
Join19001 <65016394+Join19001@users.noreply.github.com>
2022-04-10 18:57:21 +0700

    Tugas IMK 2022

---
+++ Assets/Script/CameraShaker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShaker : MonoBehaviour
+{
+    public float power = 0.7f;
+    public float duration = 1.0f;
+    public Transform cam;
+    public float slowDownAmount = 1.0f;
+
+    Vector3 startPosition;
+    float initialDuration;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.transform;
+        startPosition = cam.localPosition;
+        initialDuration = duration;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKey(KeyCode.LeftShift))
+        {
+            if(duration > 0) 
+            {
+                cam.localPosition = startPosition + Random.insideUnitSphere * power;
+                duration -= Time.deltaTime * slowDownAmount;
+            }
+            else
+            {
+                duration = initialDuration;
+                cam.localPosition = startPosition;
+            }
+        }
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Rigidbody rb;
+    public float speed = 10f;
+    [SerializeField]  float jumpHeight = 8f;
+
+    [SerializeField]  Transform groundCheck;
+    [SerializeField] LayerMask ground;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(horizontalMove * speed, rb.velocity.y, verticalMove * speed);
+
+        if (Input.GetButtonDown("Jump") && InGround())
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpHeight, rb.velocity.z);
+        }
+
+        //Run
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = 20f;
+        } else if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = 10f;
+        }
+
+        //Pause
+        if (Input.GetKey(KeyCode.Escape)) 
+        {
+            Time.timeScale = 0;
+        } else if(Input.GetKey(KeyCode.R))
+        {
+            Time.timeScale = 1;
+        }
+
+        //Action
+    }
+
+    bool InGround()
+    {
+        return Physics.CheckSphere(groundCheck.position, .1f, ground);
+    }
+}
